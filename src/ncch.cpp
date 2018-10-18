37b4df2952f36e0968d37786f1cac7afe04ff0cc
Sun Daowen <sundaowen@gmail.com>
2018-10-18 10:13:41 +0800

    copy from 3dstool

--- src/ncch.cpp
+++ src/ncch.cpp
@@ -38,10 +38,53 @@ bool CNcch::Download(bool a_bReadExtKey /* = true */)
 void CNcch::readExtKey()
 {
 	UString sExtKeyPath = UGetModuleDirName() + USTR("/ext_key.txt");
-	UPrintf(USTR("%") PRIUS USTR("\n"), sExtKeyPath.c_str());
+	FILE* fp = UFopen(sExtKeyPath.c_str(), USTR("rb"));
+	if (fp != nullptr)
+	{
+		Fseek(fp, 0, SEEK_END);
+		u32 uSize = static_cast<u32>(Ftell(fp));
+		Fseek(fp, 0, SEEK_SET);
+		char* pTxt = new char[uSize + 1];
+		fread(pTxt, 1, uSize, fp);
+		fclose(fp);
+		pTxt[uSize] = '\0';
+		string sTxt(pTxt);
+		delete[] pTxt;
+		vector<string> vTxt = SplitOf(sTxt, "\r\n");
+		for (vector<string>::const_iterator it = vTxt.begin(); it != vTxt.end(); ++it)
+		{
+			sTxt = Trim(*it);
+			if (!sTxt.empty() && !StartWith(sTxt, "//"))
+			{
+				vector<string> vExtKey = Split(sTxt, " ");
+				if (vExtKey.size() == 2)
+				{
+					if (!m_mExtKey.insert(make_pair(vExtKey[0], vExtKey[1])).second)
+					{
+						UPrintf(USTR("INFO: multiple ext key for %") PRIUS USTR("\n"), AToU(vExtKey[0]).c_str());
+					}
+				}
+				else
+				{
+					UPrintf(USTR("INFO: unknown ext key record %") PRIUS USTR("\n"), AToU(sTxt).c_str());
+				}
+			}
+		}
+	}
 }
 
 bool CNcch::writeExtKey()
 {
+	UString sExtKeyPath = UGetModuleDirName() + USTR("/ext_key.txt");
+	FILE* fp = UFopen(sExtKeyPath.c_str(), USTR("wb"));
+	if (fp == nullptr)
+	{
+		return false;
+	}
+	for (map<string, string>::const_iterator it = m_mExtKey.begin(); it != m_mExtKey.end(); ++it)
+	{
+		fprintf(fp, "%s %s\r\n", it->first.c_str(), it->second.c_str());
+	}
+	fclose(fp);
 	return false;
 }
